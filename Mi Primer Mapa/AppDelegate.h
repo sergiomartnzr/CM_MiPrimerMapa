98dcb1d0ea472b53661a704dda044e122637427f
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2017-09-27 18:22:02 -0500

    Initial Commit

---
+++ Mi Primer Mapa/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Mi Primer Mapa
+//
+//  Created by Walter Gonzalez Domenzain on 27/09/17.
+//  Copyright © 2017 wgdomenzain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Mi Primer Mapa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mi Primer Mapa
+//
+//  Created by Walter Gonzalez Domenzain on 27/09/17.
+//  Copyright © 2017 wgdomenzain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
